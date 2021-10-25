8a2a4f4936ba2bf200bdbfcc22acddaa53478bf2
JoaoFabioMA <amarall.jf@gmail.com>
2021-10-25 14:01:07 -0400

    Executando sem parar com nodemon

--- README.md
+++ README.md
@@ -2,5 +2,17 @@
 
 npm init -y
 npm install --save node-notifier
+npm install express --save
+npm install body-parser
+npm install nodemon --save
 
 echo package-lock.json >> .gitignore
+
+
+git add .
+git commit -m "Executando sem parar com nodemon"
+git push
+
+git add .
+git commit -m "v 1.0.0"
+git push
--- index.js
+++ index.js
@@ -1,16 +1,32 @@
 const notifier = require('node-notifier');
 const path = require('path');
 
+const express = require('express');
 
-notifier.notify({
-    appID: 'NodeFyjs',
-    title: 'Notificação!',
-    message: 'Primeiro funcionamento',
-    icon: path.join(__dirname + '/assets', 'exc.png'),
-    sound: true,
-    wait: true
-  },
-  function (err, response) {
-    console.log(response);
-  }
-);
+const app = express();
+
+
+//app.use(express.urlencoded())
+
+app.use(express.json())
+
+app.get('/', (request, response) => {
+    response.json({info: 'Node.js, Express, and Postgres API'});
+    notifier.notify({
+        appID: 'NodeFyjs',
+        title: 'Notificação!',
+        message: 'Alguem acessou',
+        icon: path.join(__dirname + '/assets', 'exc.png'),
+        sound: true,
+        wait: true
+      },
+      function (err, response) {
+        console.log(response);
+        //console.log('Teste');
+        console.log(request.headers);
+      }
+    );
+
+})
+
+app.listen(3000);
